2fbf64725d083dd34b51b08c39447e305afac7b3
Brian Chew <brian@bchewy.com>
2023-10-04 21:03:24 +0800

    Mongocrud

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,18 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 from pymongo import MongoClient
 from config import Config
+from datetime import datetime
 
 app = Flask(__name__)
 app.config.from_object(Config)
-client = MongoClient("localhost", 27017)
-# mongo = PyMongo(app)
+client = MongoClient(app.config['MONGO_URI'])
 
 CORS(app)
+db = client.wad2
+collection = db.routes
 
+####################### HOME endpoint #######################
 
 @app.route("/")
 def health_check():
@@ -18,6 +21,53 @@ def health_check():
             	"message": "Service is healthy."
         	}
 	), 200
+
+####################### ROUTES endpoint #######################
+# Create (POST)
+@app.route("/routes", methods=['POST'])
+def create_route():
+    data = request.json
+    data['timestamp'] = datetime.now()
+    collection.insert_one(data)
+    return jsonify({"message": "Route created successfully."}), 201
+
+# Read All (GET)
+@app.route("/routes", methods=['GET'])
+def read_all_routes():
+    all_routes = list(collection.find())
+    for route in all_routes:
+        route["_id"] = str(route["_id"])
+    return jsonify(all_routes), 200
+
+# Read One (GET)
+@app.route("/routes/<route_id>", methods=['GET'])
+def read_one_route(route_id):
+    route = collection.find_one({"route_id": route_id})
+    if route:
+        route["_id"] = str(route["_id"])
+        return jsonify(route), 200
+    else:
+        return jsonify({"message": "Route not found."}), 404
+
+# Update (PUT)
+@app.route("/routes/<route_id>", methods=['PUT'])
+def update_route(route_id):
+    data = request.json
+    updated_route = collection.find_one_and_update({"route_id": route_id}, {"$set": data})
+    if updated_route:
+        return jsonify({"message": "Route updated successfully."}), 200
+    else:
+        return jsonify({"message": "Route not found."}), 404
+
+# Delete (DELETE)
+@app.route("/routes/<route_id>", methods=['DELETE'])
+def delete_route(route_id):
+    deleted_route = collection.find_one_and_delete({"route_id": route_id})
+    if deleted_route:
+        return jsonify({"message": "Route deleted successfully."}), 200
+    else:
+        return jsonify({"message": "Route not found."}), 404
+
 
 
 if __name__ == '__main__':
--- backend/config.py
+++ backend/config.py
@@ -1,4 +1,4 @@
 class Config:
     DEBUG = True
-    SECRET_KEY = 'supersecretkey'
-    MONGO_URI = "mongodb://prd.bchwy.com:27017/wad2" #wad2 is the db/ routes is the collection.
+    # SECRET_KEY = 'supersecretkey'
+    MONGO_URI = "mongodb://admin:T%3D5T1i%2BiWUMac4sWAj!9@prd.bchwy.com:27017/?authMechanism=DEFAULT&directConnection=true"  # Replace username and password
