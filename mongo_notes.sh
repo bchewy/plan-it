61015460281a33b59b39391a655cc4e8cd7d3f81
Brian Chew <brian@bchewy.com>
2023-10-04 20:49:44 +0800

    mongo notes

---
+++ mongo_notes.sh
@@ -0,0 +1,22 @@
+# Config file lives:
+cat /etc/mongod.conf
+
+# # Start mongo
+sudo systemctl start mongod
+# # Check status
+sudo systemctl status mongod
+# # Enable mongo
+sudo systemctl enable mongod
+
+
+# Mongo admin script
+```
+use admin
+db.createUser(
+  {
+    user: "admin",
+    pwd: "T=5T1i+iWUMac4sWAj!9",
+    roles: [ { role: "userAdminAnyDatabase", db: "admin" }, "readWriteAnyDatabase" ]
+  }
+)
+```
