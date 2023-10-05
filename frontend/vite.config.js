9c967f3c29f1b09e4d70c12265ab619ee8a4ad4b
Brian Chew <brian@bchewy.com>
2023-10-05 14:03:51 +0800

    self signed cert

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,5 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import fs from 'fs';  // Import the fs module
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
@@ -15,7 +15,19 @@ export default defineConfig({
 		alias: {
 			'@': fileURLToPath(new URL('./src', import.meta.url))
 		}
+	},
+	// Remove this for self-signed certificate
+	server: {
+		https: {
+			key: fs.readFileSync('./key.pem'),
+			cert: fs.readFileSync('./cert.pem'),
+		},
+		host: 'localhost',
+		port: 5173,  // You can specify another port if you want
 	}
+	// Self signed cert above 
+
+	
 	// esbuild: {
 	// 	// Enable esbuild's node adapter 
 	// 	nodeAdapter: true
