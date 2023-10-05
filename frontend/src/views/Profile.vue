4745b6c7faca05dd033f4f8d60bbee3b05efc54a
Brian Chew <brian@bchewy.com>
2023-10-05 22:00:54 +0800

    new profile page, navbar revamp

---
+++ frontend/src/views/Profile.vue
@@ -0,0 +1,28 @@
+<script>
+import MapItem from '../components/MapItem.vue'
+import Navbar from '../components/Navbar.vue';
+import { useAuth0 } from '@auth0/auth0-vue';
+
+export default{
+	data(){
+		const { user, isAuthenticated } = useAuth0();
+		console.log('Route.vue: is authenticated?', isAuthenticated)
+		console.log('Route.vue user', user)
+		return {
+			user,
+			isAuthenticated
+		}
+	},
+	components: {
+		Navbar,
+		MapItem
+	}
+}
+</script>
+
+
+<template>
+	<Navbar />
+	<!-- <MapItem :userme="user" /> -->
+</template>
+
