d0e41b23aa3f1b228013952e18258089b3a75896
Brian Chew <brian@bchewy.com>
2023-10-17 22:08:12 +0800

    Updated backend/app.py and frontend/src/components/Navbar.vue
    In backend/app.py:
    - Added API Key validation to all routes
    - Created a new route for user creation or update
    - Added a new collection for friends in the database
    
    In frontend/src/components/Navbar.vue:
    - Updated the user authentication process
    - Added a new dropdown menu for authenticated users
    - Added a new route for user profile

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-from pymongo import MongoClient
+from pymongo import MongoClient, ReturnDocument
 from pymongo.errors import ServerSelectionTimeoutError
 from config import Config
 from datetime import datetime
@@ -13,8 +13,12 @@ app.config.from_object(Config)
 client = MongoClient(app.config['MONGO_URI'])
 
 CORS(app)
+
+# Database init
 db = client.wad2
 collection = db.routes
+user_collection = db.users
+friends_collection = db.friends
 
 # API Key
 
@@ -34,20 +38,24 @@ def require_api_key(f):
 
 
 @app.route("/")
-@require_api_key
-def health_check():
-    return jsonify(
-        {
-            "message": "Service is healthy."
-        }
-    ), 200
+def get_all_routes():
+    routes = [str(rule) for rule in app.url_map.iter_rules()]
+    return jsonify({"routes": routes}), 200
+
+# @require_api_key
+# def health_check():
+#     return jsonify(
+#         {
+#             "message": "Service is healthy."
+#         }
+#     ), 200
 
 
 # Database healthcheck
 
 
 @app.route("/db")
-@require_api_key
+# @require_api_key
 def db_check():
     client = MongoClient('localhost', serverSelectionTimeoutMS=1000)
     try:
@@ -61,7 +69,7 @@ def db_check():
 
 
 @app.route("/routes", methods=['POST'])
-@require_api_key
+# @require_api_key
 def create_route():
     data = request.json
     data['timestamp'] = datetime.now()
@@ -72,7 +80,7 @@ def create_route():
 
 
 @app.route("/routes", methods=['GET'])
-@require_api_key
+# @require_api_key
 def read_all_routes():
     all_routes = list(collection.find())
     for route in all_routes:
@@ -83,7 +91,7 @@ def read_all_routes():
 
 
 @app.route("/routes/email", methods=['GET'])
-@require_api_key
+# @require_api_key
 def read_all_routes_email():
     email = request.args.get('email')
     if email:
@@ -98,7 +106,7 @@ def read_all_routes_email():
 
 
 @app.route("/routes/<route_id>", methods=['GET'])
-@require_api_key
+# @require_api_key
 def read_one_route(route_id):
     route = collection.find_one({"route_id": route_id})
     if route:
@@ -111,7 +119,7 @@ def read_one_route(route_id):
 
 
 @app.route("/routes/<route_id>", methods=['PUT'])
-@require_api_key
+# @require_api_key
 def update_route(route_id):
     data = request.json
     updated_route = collection.find_one_and_update(
@@ -125,13 +133,72 @@ def update_route(route_id):
 
 
 @app.route("/routes/<route_id>", methods=['DELETE'])
-@require_api_key
+# @require_api_key
 def delete_route(route_id):
     deleted_route = collection.find_one_and_delete({"route_id": route_id})
     if deleted_route:
         return jsonify({"message": "Route deleted successfully."}), 200
     else:
         return jsonify({"message": "Route not found."}), 404
+
+
+@app.route("/users", methods=['POST'])
+# @require_api_key
+def create_or_update_user():
+    data = request.json
+    print(data)
+    auth0_user_id = data.get('auth0_user_id')
+    email = data.get('email')
+    # Check if user exists
+    existing_user = user_collection.find_one({"auth0_user_id": auth0_user_id})
+    if existing_user:
+        # Update user data if necessary
+        updated_user = user_collection.find_one_and_update(
+            {"auth0_user_id": auth0_user_id}, {"$set": data}, return_document=ReturnDocument.AFTER)
+        if updated_user:
+            updated_user["_id"] = str(updated_user["_id"])
+            return jsonify({"message": "User updated successfully.", "user": updated_user}), 200
+        # return jsonify({"message": "User updated successfully.", "user": updated_user}), 200
+    else:
+        # Create new user document
+        data['timestamp'] = datetime.now()
+        user_collection.insert_one(data)
+        return jsonify({"message": "User created successfully."}), 201
+
+# @app.route("/users", methods=['POST'])
+# # @require_api_key
+# def create_user():
+#     data = request.json
+#     user_collection.insert_one(data)
+#     return jsonify({"message": "User created successfully."}), 201
+
+
+@app.route("/users/search", methods=['GET'])
+# @require_api_key
+def search_users():
+    name = request.args.get('name')
+    users = list(user_collection.find({"name": name}))
+    for user in users:
+        user["_id"] = str(user["_id"])
+    return jsonify(users), 200
+
+
+@app.route("/friends", methods=['POST'])
+# @require_api_key
+def add_friend():
+    data = request.json
+    friends_collection.insert_one(data)
+    return jsonify({"message": "Friend added successfully."}), 201
+
+
+@app.route("/friends", methods=['GET'])
+# @require_api_key
+def list_friends():
+    user_id = request.args.get('user_id')
+    friends = list(friends_collection.find({"user_id": user_id}))
+    for friend in friends:
+        friend["_id"] = str(friend["_id"])
+    return jsonify(friends), 200
 
 
 if __name__ == '__main__':
--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,95 +1,161 @@
 <template>
-	<nav class="navbar navbar-expand-lg navbar-light bg-light">
-		<div class="container">
-			<a class="navbar-brand" href="#">
-				<font-awesome-icon icon="fa-solid fa-car" bounce style="color: green;" /> Plan-It
-			</a>
-			<!-- <font-awesome-icon icon="fa-solid fa-user-secret" /> -->
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+      <a class="navbar-brand" href="#">
+        <font-awesome-icon icon="fa-solid fa-car" bounce style="color: green" /> Plan-It
+      </a>
+      <!-- <font-awesome-icon icon="fa-solid fa-user-secret" /> -->
 
-			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-				aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-				<span class="navbar-toggler-icon"></span>
-			</button>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
-			<div class="collapse navbar-collapse" id="navbarNav">
-				<ul class="navbar-nav me-auto">
-					<li class="nav-item">
-						<router-link class="nav-link" to="/">Home</router-link>
-					</li>
-					<li class="nav-item">
-						<router-link class="nav-link" to="/route">Routing</router-link>
-					</li>
-					<li class="nav-item">
-						<router-link class="nav-link" to="/activity">Activity</router-link>
-					</li>
-				</ul>
-				<ul class="navbar-nav">
-					<!-- If user is not authenticated -->
-					<li v-if="!isAuthenticated" class="nav-item">
-						<a class="nav-link" @click.prevent="login">Log in</a>
-					</li>
-					<!-- <li v-if="!isAuthenticated" class="nav-item">
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav me-auto">
+          <li class="nav-item">
+            <router-link class="nav-link" to="/">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/route">Routing</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/activity">Activity</router-link>
+          </li>
+        </ul>
+        <ul class="navbar-nav">
+          <!-- If user is not authenticated -->
+          <li v-if="!isAuthenticated" class="nav-item">
+            <a class="nav-link" @click.prevent="login">Log in</a>
+          </li>
+          <!-- <li v-if="!isAuthenticated" class="nav-item">
 						<a class="nav-link" @click="signup">Sign up</a>
 					</li> -->
-					<!-- If user is authenticated -->
-					<li v-else class="nav-item dropdown">
-						<a class="nav-link dropdown-toggle d-flex align-items-center" href="#" id="navbarDropdown"
-							role="button" data-bs-toggle="dropdown" aria-expanded="false">
-							<img :src="user.picture" alt="" class="rounded-circle me-2" style="width: 30px; height: 30px;">
-							Welcome, {{ user.name }}
-						</a>
-						<ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-							<li><router-link class="dropdown-item" to="/profile">
-									<font-awesome-icon icon="fa-solid fa-circle-user" /> Profile</router-link>
-							</li>
-							<!-- <li><router-link class="dropdown-item" to="/journey">
+          <!-- If user is authenticated -->
+          <li v-else class="nav-item dropdown">
+            <a
+              class="nav-link dropdown-toggle d-flex align-items-center"
+              href="#"
+              id="navbarDropdown"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              <img
+                :src="user.picture"
+                alt=""
+                class="rounded-circle me-2"
+                style="width: 30px; height: 30px"
+              />
+              Welcome, {{ user.name }}
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li>
+                <router-link class="dropdown-item" to="/profile">
+                  <font-awesome-icon icon="fa-solid fa-circle-user" />
+                  Profile</router-link
+                >
+              </li>
+              <!-- <li><router-link class="dropdown-item" to="/journey">
 								<font-awesome-icon icon="fa-solid fa-truck-fast" />
 								Journey</router-link>
 							</li> -->
-							<li>
-								<hr class="dropdown-divider">
-							</li>
-							<li><a class="dropdown-item" @click="logout">Logout
-									<font-awesome-icon icon="fa-solid fa-arrow-right-from-bracket" />
-								</a></li>
-						</ul>
-					</li>
-				</ul>
-			</div>
-
-		</div>
-	</nav>
+              <li>
+                <hr class="dropdown-divider" />
+              </li>
+              <li>
+                <a class="dropdown-item" @click="logout"
+                  >Logout
+                  <font-awesome-icon icon="fa-solid fa-arrow-right-from-bracket" />
+                </a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
 </template>
+
+
 <script>
 import { useAuth0 } from '@auth0/auth0-vue';
 import { watch, computed, defineComponent } from 'vue';
-// import { useStore } from 'vuex';
-
 
 export default defineComponent({
 	name: 'NavBar',
 	setup() {
 		const { loginWithRedirect, user, isAuthenticated, logout } = useAuth0();
 
+		// Watch for changes in the isAuthenticated property
+		// watch(isAuthenticated, async (newValue) => {
+		// 	// If the user is authenticated
+		// 	if (newValue) {
+		// 		// Create or update the user in your database
+		// 		try {
+		// 			const response = await fetch('http://127.0.0.1:5000/users', {
+		// 				method: 'POST',
+		// 				headers: {
+		// 					'Content-Type': 'application/json'
+		// 				},
+		// 				body: JSON.stringify({
+		// 					auth0_user_id: user.value.sub,
+		// 					email: user.value.email
+		// 				})
+		// 			});
+		// 			const data = await response.json();
+		// 			console.log('User upserted:', data);
+		// 		} catch (e) {
+		// 			console.error('Failed to upsert user:', e);
+		// 		}
+		// 	}
+		// });
+		watch(user, async (newValue) => {
+			// If the user is authenticated
+			if (newValue) {
+				// Create or update the user in your database
+				try {
+					const response = await fetch('http://127.0.0.1:5000/users', {
+						method: 'POST',
+						headers: {
+							'Content-Type': 'application/json'
+						},
+						body: JSON.stringify({
+							auth0_user_id: newValue.sub,
+							email: newValue.email
+						})
+					});
+					const data = await response.json();
+					console.log('User upserted:', data);
+				} catch (e) {
+					console.error('Failed to upsert user:', e);
+				}
+			}
+		});
+		
 		return {
 			login: async () => {
-				console.log('Login button clicked');
 				try {
-					await loginWithRedirect();
+					await loginWithRedirect({
+						appState: { targetUrl: window.location.pathname }
+					});
 				} catch (e) {
 					console.error('Failed to login:', e);
 				}
 			},
 			logout: () => {
 				logout({ logoutParams: { returnTo: window.location.origin } });
-				// store.dispatch('updateUser', null);
 			},
-			// user: computed(() => store.getters.user),
 			user,
 			isAuthenticated,
-			// isAuthenticated: computed(() => store.getters.isAuthenticated),
 		};
 	}
 });
-
 </script>
