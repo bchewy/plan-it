85aacb424bc87ca011b2b85bce79031db921db53
Brian Chew <brian@bchewy.com>
2023-10-20 08:44:42 +0800

    Changed some code

--- backend/app.py
+++ backend/app.py
@@ -19,6 +19,7 @@ db = client.wad2
 collection = db.routes
 user_collection = db.users
 friends_collection = db.friends
+friend_requests_collection = db.friend_requests  # New collection for friend requests
 
 # API Key
 
@@ -226,5 +227,51 @@ def list_friends():
     return jsonify(friends), 200
 
 
+@app.route("/friend_requests", methods=['POST'])
+@require_api_key
+def send_friend_request():
+    data = request.json
+    sender_id = data.get('sender_id')
+    sender_email = data.get('sender_email')
+    receiver_email = data.get('receiver_email')
+
+    # Check if the user is trying to send a request to themselves
+    if sender_email == receiver_email:
+        return jsonify({"message": "You can't send a friend request to yourself!"}), 400
+
+    # Check if receiver exists in the users database
+    receiver = user_collection.find_one({"email": receiver_email})
+    if not receiver:
+        return jsonify({"message": "Receiver not found in users database."}), 404
+
+    # Check if friend request is already sent
+    sender = friend_requests_collection.find_one({"sender_id": sender_id})
+    if sender and receiver_email in sender.get('sent_requests', []):
+        return jsonify({"message": "Friend request already sent."}), 400
+
+    # Send friend request
+    friend_requests_collection.update_one(
+        {"sender_id": sender_id}, 
+        {"$set": {"sender_email": sender_email}, "$push": {"sent_requests": receiver_email}}, 
+        upsert=True
+    )
+    return jsonify({"message": "Friend request sent successfully."}), 201
+
+
+@app.route("/friend_requests", methods=['GET'])
+@require_api_key
+def list_friend_requests():
+    email = request.args.get('email')
+    user = friend_requests_collection.find_one({"sender_email": email})
+    if user:
+        sent_requests = user.get('sent_requests', [])
+    else:
+        sent_requests = []
+    return jsonify(sent_requests), 200
+
+
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8888, debug=True)  # must be same as gunicorn
+
