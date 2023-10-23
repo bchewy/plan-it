a00a6f824e588420b2acc6b475faf8e523792b90
ryan99324 <ryanlee99324@gmail.com>
2023-10-24 01:26:51 +0800

    e

--- frontend/src/views/Community.vue
+++ frontend/src/views/Community.vue
@@ -20,8 +20,8 @@
 	<hr>
     <li class="nav-item dropdown"><a class="h3 nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown">Groups</a>
 	<ul class="dropdown-menu bg-light">
-		<li><a class="dropdown-item" href="#"><font-awesome-icon icon="fa-solid fa-plus"/> Create group</a></li>
-		<li><a class="dropdown-item" href="#"><font-awesome-icon icon="fa-solid fa-users"/>  Your groups</a></li>
+		<li><a class="dropdown-item" href="#"><font-awesome-icon icon="fa-solid fa-plus"/>   Create group</a></li>
+		<li><a class="dropdown-item" href="#"><font-awesome-icon icon="fa-solid fa-users"/>   Your groups</a></li>
 	</ul>
 	</li>
 	<hr>
