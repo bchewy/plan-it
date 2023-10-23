e4dfdfca1e9bba025f2c3d8a4f09515e8ddb427b
ryan99324 <ryanlee99324@gmail.com>
2023-10-23 16:36:57 +0800

    sidebar test

--- frontend/src/views/Community.vue
+++ frontend/src/views/Community.vue
@@ -5,16 +5,35 @@
 
 	</header>
     <body>
-    <div class="row justify-content-center">
-        <div class="text-center h1 text-muted mt-3">Community</div>
+	<div class="container-fluid">
+		<div class="card flex justify-content-center">
+    <Sidebar v-model:visible="visible">
+        <h2>Community</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    </Sidebar>
+    <Button icon="pi pi-arrow-right" @click="visible = true" />
+		</div>
 
-    </div>
+	
+
+				</div>
+
+    
+        
+	
+
+    
     </body>
 	<footer>
 		<!-- footer -->
 	</footer>
 </template>
 <script>
+import { createApp } from 'vue';
+import PrimeVue from 'primevue/config';
+import Sidebar from 'primevue/sidebar';
+const app = createApp(App);
+app.use(PrimeVue);
 import NavBar from "../components/Navbar.vue";
 // import Login from '../components/Login.vue'
 import { useAuth0 } from '@auth0/auth0-vue';
