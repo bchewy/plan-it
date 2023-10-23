22debf31061c38d30d74c819f4b9913ea3b94c3b
ryan99324 <ryanlee99324@gmail.com>
2023-10-23 16:46:33 +0800

    sidebar

--- frontend/src/views/Community.vue
+++ frontend/src/views/Community.vue
@@ -5,18 +5,24 @@
 
 	</header>
     <body>
-	<div class="container-fluid">
-		<div class="card flex justify-content-center">
-    <Sidebar v-model:visible="visible">
-        <h2>Community</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-    </Sidebar>
-    <Button icon="pi pi-arrow-right" @click="visible = true" />
-		</div>
+<nav class="navbar bg-light">
 
-	
+<div class="container-fluid">
 
-				</div>
+  <ul class="navbar-nav">
+	<li class="nav-item">
+	  <a class="nav-link" href="#">For you page</a>
+	</li>
+	<li class="nav-item">
+	  <a class="nav-link" href="#">Your groups</a>
+	</li>
+	<li class="nav-item">
+	  <a class="nav-link" href="#">Your friends</a>
+	</li>
+  </ul>
+</div>
+
+</nav>
 
     
         
@@ -29,11 +35,7 @@
 	</footer>
 </template>
 <script>
-import { createApp } from 'vue';
-import PrimeVue from 'primevue/config';
-import Sidebar from 'primevue/sidebar';
-const app = createApp(App);
-app.use(PrimeVue);
+
 import NavBar from "../components/Navbar.vue";
 // import Login from '../components/Login.vue'
 import { useAuth0 } from '@auth0/auth0-vue';
