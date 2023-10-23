241c6828923c1b6ab96092d6e0e7a6a5685bbc3f
ryan99324 <ryanlee99324@gmail.com>
2023-10-23 19:06:46 +0800

    icon

--- frontend/src/views/Community.vue
+++ frontend/src/views/Community.vue
@@ -32,7 +32,7 @@
 <div class="row justify-content-center">
 <div class="col-8 rounded border bg-white">
 	<input type="text" class="mt-3 form-control" placeholder="Share something with the community!">
-	<button class="btn btn-primary" type="button"><span class="plus-circle-fill"></span></button>
+	<button class="btn btn-primary" type="button"><i class="bi bi-plus-circle-fill"></i></button>
 </div>
 </div>
 </div>
