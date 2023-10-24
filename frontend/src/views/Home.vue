4a5b5a34997da9f87f3f41d74e5e25ce925be29f
saphalex <145246552+saphalex@users.noreply.github.com>
2023-10-24 23:20:57 +0800

    Update Home.vue

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,20 +1,45 @@
 <template>
+	<header>
 		<!-- nav -->
 		<NavBar />
 
+	</header>
 
-<body>
-<div class=" text-center" style="margin-bottom:0">
-  <h1>PLAN-IT!</h1>
-  <p>Singapore's Best Carbon-efficient Travel Planner</p> 
-</div>
+  <div>
+    <p class="text-center text-capitalize fs-1 fw-bold">Carbon-First Routing</p>
+    <p class="text-secondary">Account for your own carbon footprint for your traffic taken, value-add and commit to reducing your carbon footprint and reduce your personal impact.</p>
+    <a href="https://wad2.bchwy.com/route" class="btn text-uppercase text-light fw-bold px-sm-4 py-sm-3 Route">Route Now!</a>
+  </div>
 
-</body>
+<style scoped>
+div {
+  padding: 8px;
+  position: relative;
+}
+
+p:first-child {
+  color: #FFFF;
+}
+
+p {
+  text-align: justify;
+  line-height: 1.6;
+}
+
+.Route {
+  background-color: rgb(209, 244, 209); /* Change the button color */
+  border-radius: 10px; /* Add rounded corners */
+  transition: outline 0.3s ease-in-out; /* Add transition for outline */
+}
+
+.Route:hover {
+  outline: 2px solid #00FF00; /* Green outline on hover */
+}
+</style>
+
 	<footer>
 		<!-- footer -->
 	</footer>
-</template>
-
 
 <script>
 import NavBar from '../components/Navbar.vue'
