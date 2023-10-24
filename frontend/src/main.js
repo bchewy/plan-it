c88b2d2da3cbc6c54c3e9a83f192a5991a195dda
Brian Chew <brian@bchewy.com>
2023-10-24 14:00:24 +0800

    Fixed an issue with fawmi/vue-google-maps latest version causing an issue with fast-deep-equal

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -10,7 +10,7 @@ import App from './App.vue'
 import router from './router'
 
 // Google Maps
-// import VueGoogleMaps from '@fawmi/vue-google-maps'
+import VueGoogleMaps from '@fawmi/vue-google-maps'
 
 // Auth0
 import { createAuth0 } from '@auth0/auth0-vue';
@@ -43,15 +43,15 @@ import PrimeVue from 'primevue/config';
 
 
 const app = createApp(App)
-// app.use(VueGoogleMaps, {
-// 	load: {
-// 		key: "AIzaSyC6xTDY_NrDH0U1NSE2Ug6AnzuVsbRPFYM",
-// 		libraries: "places",
-// 		componentRestrictions: {
-// 			country: "SG"
-// 		}
-// 	}
-// })
+app.use(VueGoogleMaps, {
+	load: {
+		key: "AIzaSyC6xTDY_NrDH0U1NSE2Ug6AnzuVsbRPFYM",
+		libraries: "places",
+		componentRestrictions: {
+			country: "SG"
+		}
+	}
+})
 app.use(
 	createAuth0({
 		domain: authConfig.domain,
