0a0e2e8c5d18940b515eac4f136ecf8aa7c4c947
Brian Chew <brian@bchewy.com>
2023-10-24 14:13:26 +0800

    passing over to child share badge component

--- frontend/src/components/Share_badge.vue
+++ frontend/src/components/Share_badge.vue
@@ -1,27 +1,26 @@
 <template>
-
-<div v-if="badges" class="d-flex justify-content-center align-items-center">
-					<div class="modal fade" id="badgesOverlay" tabindex="-1" aria-labelledby="badgesOverlayLabel" aria-hidden="true">
-						<div class="modal-dialog">
-							<div class="modal-content">
-								<div class="modal-header">
-									<h5 class="modal-title" id="makePostModalLabel">Badges</h5>
-									<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-								</div>
-								<div class="modal-body">
-									<ul>
-                                        <li> Badge1 </li> <input type="radio">
-                                    </ul>
-								</div>
-								<div class="modal-footer">
-									<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-									<button type="button" class="btn btn-primary" @click="confirmShareBadge">Confirm</button> <!--yet to define-->
-								</div>
-							</div>
-						</div>
+	<div v-if="badges" class="d-flex justify-content-center align-items-center">
+		<div class="modal fade" id="badgesOverlay" tabindex="-1" aria-labelledby="badgesOverlayLabel" aria-hidden="true">
+			<div class="modal-dialog">
+				<div class="modal-content">
+					<div class="modal-header">
+						<h5 class="modal-title" id="makePostModalLabel">Badges</h5>
+						<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
 					</div>
-					<button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#badgesOverlay">Make a Post</button>
+					<div class="modal-body">
+						<ul>
+							<li> Badge1 </li> <input type="radio">
+						</ul>
+					</div>
+					<div class="modal-footer">
+						<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+						<button type="button" class="btn btn-primary" @click="confirmShareBadge">Confirm</button> <!--yet to define-->
+					</div>
 				</div>
+			</div>
+		</div>
+		<button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#badgesOverlay">Make a Post</button>
+	</div>
 </template>
 
 <script>
@@ -30,17 +29,17 @@ import axios from "axios";
 import { useAuth0 } from '@auth0/auth0-vue';
 
 export default defineComponent({
+	props: ['badges'],
+	setup() {
+		const { user, isAuthenticated } = useAuth0();
+		// const badges = false;
+		return {
+			// badges,
+			isAuthenticated,
+			user
 
-setup(){
-    const { user, isAuthenticated } = useAuth0();
-    const badges=false;
-    return{
-        badges,
-        isAuthenticated,
-        user
-
-    }
-}
+		}
+	}
 
 })
 
--- frontend/src/views/Community.vue
+++ frontend/src/views/Community.vue
@@ -1,65 +1,64 @@
 <template>
+	<!-- nav -->
+	<NavBar />
 
-		<!-- nav -->
-		<NavBar />
+
+	<body class="position-relative">
+		<div class="container-fluid">
+			<div class="row">
+
+				<nav class="col-2 position-fixed h-100" style="background-color: rgb(209, 244, 209)">
+
+					<div class="container-fluid">
+						<br>
+						<span class="h5 text-muted">Plan-it Community</span>
+
+						<ul class="navbar-nav flex-column mt-5">
+
+							<li class="active nav-item"><a class="h3 nav-link" href="#">For you</a></li>
+							<hr>
+							<li class="nav-item dropdown"><a class="h3 nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown">Groups</a>
+								<ul class="dropdown-menu bg-light">
+									<li><a class="dropdown-item" href="#"><font-awesome-icon icon="fa-solid fa-plus" /> Create group</a></li>
+									<li><a class="dropdown-item" href="#"><font-awesome-icon icon="fa-solid fa-users" /> Your groups</a></li>
+								</ul>
+							</li>
+							<hr>
+							<li class="nav-item"><a class="h3 nav-link" href="#">Friends</a></li>
+							<hr>
+
+						</ul>
+					</div>
+				</nav>
+				<div class="col-2"></div>
+				<div class="col-10 h-100" style="background-color: rgb(232, 251, 240);">
+
+					<h1 class="text-center py-3 text-muted" style="font-weight: bold;">For You Page</h1>
+					<div class="row justify-content-center">
+						<div class="col-8 rounded border bg-white">
+							<textarea class="my-3 form-control" placeholder="Share something with the community!"></textarea>
+							<div class="row mb-3 justify-content-between">
+								<span class="col-1"></span>
+
+								<button class="btn btn-secondary col-3" type="button" @click="badges = !badges"><font-awesome-icon icon="fa-solid fa-share" /> Share a badge</button>
+								<Share_badge :badges="badges"></Share_badge>
+								<button class="btn btn-secondary col-3" type="button"><font-awesome-icon icon="fa-solid fa-tag" /> Tag a friend</button>
+								<button class="btn btn-secondary col-3" type="button"><font-awesome-icon icon="fa-solid fa-plus-square" /> Create post!</button>
+								<span class="col-1"></span>
+							</div>
+
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 
 
-<body class="position-relative" >
-<div class="container-fluid">
-<div class="row">
 
-<nav class="col-2 position-fixed h-100"  style="background-color: rgb(209, 244, 209)">
-	
-	<div class="container-fluid">
-		<br>
-		<span class="h5 text-muted">Plan-it Community</span>
-		
-	<ul class="navbar-nav flex-column mt-5">
-	
-    <li class="active nav-item"><a class="h3 nav-link" href="#">For you</a></li>
-	<hr>
-    <li class="nav-item dropdown"><a class="h3 nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown">Groups</a>
-	<ul class="dropdown-menu bg-light">
-		<li><a class="dropdown-item" href="#"><font-awesome-icon icon="fa-solid fa-plus"/>   Create group</a></li>
-		<li><a class="dropdown-item" href="#"><font-awesome-icon icon="fa-solid fa-users"/>   Your groups</a></li>
-	</ul>
-	</li>
-	<hr>
-    <li class="nav-item"><a class="h3 nav-link" href="#">Friends</a></li>
-	<hr>
 
-</ul>
-	</div>
-</nav>
-<div class="col-2"></div>
-<div class="col-10 h-100" style="background-color: rgb(232, 251, 240);">
-	
-<h1 class="text-center py-3 text-muted" style="font-weight: bold;">For You Page</h1>
-<div class="row justify-content-center">
-<div class="col-8 rounded border bg-white">
-	<textarea class="my-3 form-control" placeholder="Share something with the community!"></textarea>
-	<div class="row mb-3 justify-content-between">
-	<span class="col-1"></span>
-	
-	<button class="btn btn-secondary col-3" type="button" @click="badges=!badges"><font-awesome-icon icon="fa-solid fa-share"/>  Share a badge</button>
-	<Share_badge></Share_badge>
-	<button class="btn btn-secondary col-3" type="button"><font-awesome-icon icon="fa-solid fa-tag"/>  Tag a friend</button>
-	<button class="btn btn-secondary col-3" type="button"><font-awesome-icon  icon="fa-solid fa-plus-square"/>  Create post!</button>
-	<span class="col-1"></span>
-	</div>
 
-</div>
-</div>
-</div>
-</div>
-</div>
 
-    
-        
-	
-
-    
-    </body>
+	</body>
 	<footer>
 		<!-- footer -->
 	</footer>
@@ -67,22 +66,22 @@
 <script>
 
 import NavBar from "../components/Navbar.vue";
-// import Login from '../components/Login.vue'
 import { useAuth0 } from '@auth0/auth0-vue';
 import Share_badge from "../components/Share_badge.vue";
-export default {
+import { defineComponent, ref } from 'vue';
+export default defineComponent({
 	name: 'Community',
-	data(){
+	props: ['badges'],
+	data() {
 
 	},
 	components: {
-		NavBar,  // Register the NavBar component
-		// Login
+		NavBar,
 		Share_badge
 	},
 	setup() {
 		const { loginWithRedirect, user, isAuthenticated } = useAuth0();
-		const badges=false
+		const badges = ref(false);
 
 		console.log('Setup method is called');
 
@@ -91,8 +90,6 @@ export default {
 				console.log('Login button clicked');
 				try {
 					await loginWithRedirect();
-					// console.log('User:', user);
-					// console.log('Authenticated:', isAuthenticated);
 				} catch (e) {
 					alert('Failed to login');
 					console.error('Failed to login:', e);
@@ -103,5 +100,5 @@ export default {
 			badges
 		};
 	}
-}
+})
 </script>
