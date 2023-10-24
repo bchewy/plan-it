ef8a2976931406492126f598da69e513aba3691e
saphalex <145246552+saphalex@users.noreply.github.com>
2023-10-25 02:37:44 +0800

    Update Home.vue

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,82 +1,72 @@
-
 <template>
-  <div>
-    <header>
-      <!-- nav -->
-      <NavBar />
-    </header>
+	<header>
+	  <!-- nav -->
+	  <NavBar />
+	</header>
+  
+	<!--showcase-->
+	<section class="p-5 text-center">
+	  <div class="container" style= "background-color: rgb(209, 244, 209);">
+			<h1>Plan-It!<br> Stay <span class= "text-success"> Sustainable </span> . </h1>
+	  </div>
+	</section>
 
-    <div>
-      <p class="text-center text-capitalize fs-1 fw-bold">Carbon-First Routing</p>
-      <p class="text-secondary">Account for your own carbon footprint for your traffic taken, value-add and commit to reducing your carbon footprint and reduce your personal impact.</p>
-      <a href="https://wad2.bchwy.com/route" class="btn text-uppercase text-light fw-bold px-sm-4 py-sm-3 Route">Route Now!</a>
-    </div>
-
-    <footer>
-      <!-- footer -->
-    </footer>
-  </div>
-</template>
-
-<style>
-  div {
-    padding: 8px;
-    position: relative;
-  }
-
-  p:first-child {
-    color: black;
-  }
-
-  p {
-    text-align: justify;
-    line-height: 1.6;
-  }
-
-  .Route {
-    background-color: #00B300; /* Change the button color */
-    border-radius: 10px; /* Add rounded corners */
-    transition: outline 0.3s ease-in-out; /* Add transition for outline */
-  }
-
-  .Route:hover {
-    outline: 2px solid #00B300; /* Green outline on hover */
-  }
-</style>
-
+	<!-- signup now-->
+	<section class=" bg-success text-light p-5">
+		<div class = "container">
+			<div class="d-md-flex justify-content-between-align-items-center">
+				<h3 class = "mb-3 mb-md-0"> Refer a friend now!</h3>
+				<div class = "input-group">
+					<input
+					type = "text"
+					class = "form-control"
+					placeholder = "Friend's Email Address"
+					aria-label="Friend's Email Address"
+					aria-describedby="button-addon2">
+					<button class=" btn btn-lg" 
+					type = "button" 
+					id = "button-addon2"> Send </button>
+				</div>
+			</div>
+		</div>
+	</section>
+  
+	<footer>
+	  <!-- footer -->
+	</footer>
+  </template>
+  
 <script>
 import NavBar from '../components/Navbar.vue'
 // import Login from '../components/Login.vue'
 import { useAuth0 } from '@auth0/auth0-vue';
 
 export default {
-  name: 'Home',
-  components: {
-    NavBar,  // Register the NavBar component
-    // Login
-  },
-  setup() {
-    const { loginWithRedirect, user, isAuthenticated } = useAuth0();
+	name: 'Home',
+	components: {
+		NavBar,  // Register the NavBar component
+		// Login
+	},
+	setup() {
+		const { loginWithRedirect, user, isAuthenticated } = useAuth0();
 
-    console.log('Setup method is called');
+		console.log('Setup method is called');
 
-    return {
-      login: async () => {
-        console.log('Login button clicked');
-        try {
-          await loginWithRedirect();
-          // console.log('User:', user);
-          // console.log('Authenticated:', isAuthenticated);
-        } catch (e) {
-          alert('Failed to login');
-          console.error('Failed to login:', e);
-        }
-      },
-      user,
-      isAuthenticated,
-    };
-  }
+		return {
+			login: async () => {
+				console.log('Login button clicked');
+				try {
+					await loginWithRedirect();
+					// console.log('User:', user);
+					// console.log('Authenticated:', isAuthenticated);
+				} catch (e) {
+					alert('Failed to login');
+					console.error('Failed to login:', e);
+				}
+			},
+			user,
+			isAuthenticated,
+		};
+	}
 }
 </script>
-
-
