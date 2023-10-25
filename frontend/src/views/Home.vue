ef6753321ddc98b3ca6fa248c921ca12bcfedd71
saphalex <145246552+saphalex@users.noreply.github.com>
2023-10-25 17:07:45 +0800

    Update Home.vue

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -97,46 +97,60 @@
     </div>
   </section>
 
-  <!-- Question Accordion -->
-  <section id = "questions" class = "p-5">
+    <!-- Question Accordion -->
+	<section id = "questions" class = "p-5">
 	<div class = "container">
 		<h2 class = "text-center mb-4">Frequently Asked Questions</h2>
-		<div class="accordion accordion-flush" id="accordionFlushExample">
+		<div class="accordion accordion-flush" id="questions">
+	<!--Item 1-->
   <div class="accordion-item">
-    <h2 class="accordion-header" id="flush-headingOne">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-        Accordion Item #1
+    <h2 class="accordion-header">
+      <button 
+	  class="accordion-button collapsed" 
+	  type="button" 
+	  data-bs-toggle="collapse" 
+	  data-bs-target="#question-one">
+        Q1
       </button>
     </h2>
-    <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
-      <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the first item's accordion body.</div>
+    <div id="question-one" class="accordion-collapse collapse" data-bs-parent="#questions">
+      <div class="accordion-body">Add answers later</div>
     </div>
   </div>
-  <div class="accordion-item">
-    <h2 class="accordion-header" id="flush-headingTwo">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
-        Accordion Item #2
+	<!--Item 2-->
+	<div class="accordion-item">
+    <h2 class="accordion-header">
+      <button 
+	  class="accordion-button collapsed" 
+	  type="button" 
+	  data-bs-toggle="collapse" 
+	  data-bs-target="#question-two">
+        Q2
       </button>
     </h2>
-    <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
-      <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the second item's accordion body. Let's imagine this being filled with some actual content.</div>
+    <div id="question-two" class="accordion-collapse collapse" data-bs-parent="#questions">
+      <div class="accordion-body">Add answers later</div>
     </div>
   </div>
-  <div class="accordion-item">
-    <h2 class="accordion-header" id="flush-headingThree">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
-        Accordion Item #3
+  	<!--Item 3-->
+	  <div class="accordion-item">
+    <h2 class="accordion-header">
+      <button 
+	  class="accordion-button collapsed" 
+	  type="button" 
+	  data-bs-toggle="collapse" 
+	  data-bs-target="#question-three">
+        Q3
       </button>
     </h2>
-    <div id="flush-collapseThree" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
-      <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the third item's accordion body. Nothing more exciting happening here in terms of content, but just filling up the space to make it look, at least at first glance, a bit more representative of how this would look in a real-world application.</div>
+    <div id="question-three" class="accordion-collapse collapse" data-bs-parent="#questions">
+      <div class="accordion-body">Add answers later</div>
     </div>
   </div>
 </div>
+</div>
+</section>
 
-
-	</div>
-  </section>
 
 
 	<footer>
