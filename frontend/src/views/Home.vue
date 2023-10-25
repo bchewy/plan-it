2093d928730119b46c5be3264101fa63bc34f597
saphalex <145246552+saphalex@users.noreply.github.com>
2023-10-25 17:32:15 +0800

    Update Home.vue

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -4,12 +4,12 @@
 		<NavBar />
 	</header>
 
-	<!--showcase-->
-	<section class="text-center col-12 m-0 p-0">
-		<div class="container" style="background-color: rgb(209, 244, 209);">
-			<h1>Plan-It!</h1>
-		</div>
-	</section>
+<!--showcase-->
+<section class="text-center col-12 m-0 p-0" style="background-color: rgb(209, 244, 209); margin: 0; padding: 0;">
+  <div class="container" style="max-width: 100%; padding: 0;">
+    <h1>Plan-It!</h1>
+  </div>
+</section>
 
 	<!-- signup now-->
 	<section class="bg-success text-light">
@@ -153,8 +153,15 @@
 
 
 
-	<footer>
+	<footer class="p-5 text-center position-relative" style = "background-color: rgb(209, 244, 209);">
 		<!-- footer -->
+		<div class = "container">
+			<p class = "lead"> Copyright &copy; 2023 wad2winners</p>
+			<a href="#" class="position-absolute bottom-0 end-0 p-5">
+				<i class="bi bi-arrow-up-circle h1" style="color: green;"></i>
+			</a>
+		</div>
+
 	</footer>
 </template>
   
