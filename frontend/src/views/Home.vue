9e2784cbc7fcf95df583597d538aedff98fe0c81
saphalex <145246552+saphalex@users.noreply.github.com>
2023-10-25 17:41:54 +0800

    Update Home.vue

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -151,12 +151,12 @@
 </div>
 </section>
 
-<footer class="p-5 text-center position-relative" style="background-color: rgb(209, 244, 209);">
+<footer class="p-5 text-center position-relative bg-success">
   <!-- footer -->
   <div class="container">
     <p class="lead">Copyright &copy; 2023 wad2winners</p>
     <a href="#" class="position-absolute bottom-0 end-0 p-5 text-center">
-      <i class="bi bi-arrow-up-circle h1 text-success" style="background-color: transparent;"></i>
+      <i class="bi bi-arrow-up-circle h1 text-muted" style="background-color: transparent;"></i>
     </a>
   </div>
 </footer>
