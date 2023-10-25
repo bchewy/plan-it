4932326a51cb22ef9e8052d5ed9311d8be07089e
saphalex <145246552+saphalex@users.noreply.github.com>
2023-10-25 16:13:44 +0800

    Update Home.vue

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -38,8 +38,8 @@
 				<img class="img-fluid w-50" src="@/assets/homepage/co2.jpg" alt="">
 			</div>
 		</div>
-		<h1>Plan-It!</h1>
-	</section>
+	<h3 v-if="isAuthenticated">Hi, {{ userName }} !</h3> <!-- Display the user's name if authenticated -->
+</section>
 
 	<!--Boxes-->
 	<section class="p-5">
@@ -107,40 +107,36 @@
 </template>
   
 <script>
-
-import NavBar from '../components/Navbar.vue'
-// import Login from '../components/Login.vue'
+import NavBar from '../components/Navbar.vue';
 import { useAuth0 } from '@auth0/auth0-vue';
 
 export default {
-	name: 'Home',
-	components: {
-		NavBar,  // Register the NavBar component
+  name: 'Home',
+  components: {
+	NavBar,
+  },
+  setup() {
+	const { loginWithRedirect, user, isAuthenticated } = useAuth0();
+	const userName = isAuthenticated ? user.name : ''; // Get the user's name if authenticated
 
-		// Login
-	},
-	setup() {
-		const { loginWithRedirect, user, isAuthenticated } = useAuth0();
+	console.log('Setup method is called');
 
-		console.log('Setup method is called');
-
-		return {
-			login: async () => {
-				console.log('Login button clicked');
-				try {
-					await loginWithRedirect();
-					// console.log('User:', user);
-					// console.log('Authenticated:', isAuthenticated);
-				} catch (e) {
-					alert('Failed to login');
-					console.error('Failed to login:', e);
-				}
-			},
-			user,
-			isAuthenticated,
-		};
-	}
-}
+	return {
+	  login: async () => {
+		console.log('Login button clicked');
+		try {
+		  await loginWithRedirect();
+		} catch (e) {
+		  alert('Failed to login');
+		  console.error('Failed to login:', e);
+		}
+	  },
+	  user,
+	  isAuthenticated,
+	  userName, // Add user's name to the return object
+	};
+  },
+};
 </script>
 
 <style scoped>
