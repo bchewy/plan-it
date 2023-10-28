97cb5c1523ac1481831156ef7b96b4824a0ceb22
Brian Chew <brian@bchewy.com>
2023-10-29 01:58:23 +0800

    Moved community sidebar into a component

---
+++ frontend/src/components/CommunitySidebar.vue
@@ -0,0 +1,21 @@
+<template>
+	<nav class="col-2 position-fixed h-100">
+		<div class="container-fluid">
+			<br>
+			<span class="h5 text-muted">Plan-it Community</span>
+			<ul class="navbar-nav flex-column mt-5">
+				<li class="active nav-item"><router-link class="h3 nav-link" to="/community">For you</router-link></li>
+				<hr>
+				<li class="nav-item dropdown"><a class="h3 nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown">Groups</a>
+					<ul class="dropdown-menu bg-light">
+						<li><router-link class="dropdown-item" to="/community/createGroup"><font-awesome-icon icon="fa-solid fa-plus" /> Create group</router-link></li>
+						<li><router-link class="dropdown-item" to="/community/yourGroups"><font-awesome-icon icon="fa-solid fa-users" /> Your groups</router-link></li>
+					</ul>
+				</li>
+				<hr>
+				<li class="nav-item"><router-link class="h3 nav-link" to="/community/friendCommunity">Friends</router-link></li>
+				<hr>
+			</ul>
+		</div>
+	</nav>
+</template>
--- frontend/src/views/Community.vue
+++ frontend/src/views/Community.vue
@@ -1,62 +1,33 @@
+<style scoped>
+.container-fluid {
+	min-height: 100vh;
+	padding-top: 3rem;
+	background-color: #d1f4d1;
+}
+</style>
 <template>
 	<!-- nav -->
 	<NavBar />
-	<div v-if="isLoading || !isAuthenticated" class="loading-screen">
-		<div v-if="isLoading">Loading...</div>
-		<div v-else class="row justify-content-center align-items-center" style="height: 100vh">
-			<h3 class="text-center p-5">Please log in to use this feature</h3>
-		</div>
-	</div>
-
-	<div v-else class="position-relative">
+	<div class="container-fluid pt-3 bg-supergreen pb-3">
 		<div class="container-fluid" style="background-color: #d1f4d1">
 			<div class="row">
-
-				<nav class="col-2 position-fixed h-100">
-
-					<div class="container-fluid">
-						<br>
-						<span class="h5 text-muted">Plan-it Community</span>
-
-						<ul class="navbar-nav flex-column mt-5">
-
-							<li class="active nav-item"><router-link class="h3 nav-link" to="/community">For you</router-link></li>
-							<hr>
-							<li class="nav-item dropdown"><a class="h3 nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown">Groups</a>
-								<ul class="dropdown-menu bg-light">
-									<li><router-link class="dropdown-item" to="/community/createGroup"><font-awesome-icon icon="fa-solid fa-plus" /> Create group</router-link></li>
-									<li><router-link class="dropdown-item" to="/community/yourGroups"><font-awesome-icon icon="fa-solid fa-users" /> Your groups</router-link></li>
-								</ul>
-							</li>
-							<hr>
-							<li class="nav-item"><router-link class="h3 nav-link" to="/community/friendCommunity">Friends</router-link></li>
-							<hr>
-
-						</ul>
-					</div>
-				</nav>
+				<CommunitySidebar></CommunitySidebar>
 				<div class="col-2"></div>
 				<div class="col-10 h-100" style="background-color: rgb(232, 251, 240);">
-
 					<h1 class="text-center py-3 text-muted" style="font-weight: bold;">For You Page</h1>
 					<div class="row justify-content-center">
 						<div class="col-8 rounded border bg-white">
 							<textarea class="my-3 form-control" placeholder="Share something with the community!"></textarea>
 							<div class="row mb-3 justify-content-between">
 								<span class="col-1"></span>
-
-
 								<Share_badge></Share_badge>
 								<button class="btn btn-secondary col-3" type="button"><font-awesome-icon icon="fa-solid fa-tag" /> Tag a friend</button>
 								<button class="btn btn-secondary col-3" type="button"><font-awesome-icon icon="fa-solid fa-plus-square" /> Create post!</button>
 								<span class="col-1"></span>
 							</div>
-
 						</div>
 					</div>
-
-					<div class="row justify-content-center mt-5">
-
+					<div class="row justify-content-center my-4">
 						<div class="col-8 rounded border bg-white"><font-awesome-icon icon="fa-solid fa-user" class="mt-3"></font-awesome-icon> User<br><br>
 							<heart_btn></heart_btn>
 							<p>Post text</p>
@@ -71,7 +42,7 @@
 	</footer>
 </template>
 <script>
-
+import CommunitySidebar from "../components/CommunitySidebar.vue";
 import NavBar from "../components/Navbar.vue";
 import { useAuth0 } from '@auth0/auth0-vue';
 import heart_btn from "../components/like_button.vue";
@@ -82,10 +53,10 @@ export default {
 
 	},
 	components: {
-		NavBar,  // Register the NavBar component
-		// Login
+		NavBar,
 		Share_badge,
-		heart_btn
+		heart_btn,
+		CommunitySidebar
 	},
 	setup() {
 		const { loginWithRedirect, user, isAuthenticated } = useAuth0();
@@ -98,8 +69,6 @@ export default {
 				console.log('Login button clicked');
 				try {
 					await loginWithRedirect();
-					// console.log('User:', user);
-					// console.log('Authenticated:', isAuthenticated);
 				} catch (e) {
 					alert('Failed to login');
 					console.error('Failed to login:', e);
