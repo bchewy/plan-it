da9b43ad8daf1160eef501ea33bdccb1336f47cd
saphalex <145246552+saphalex@users.noreply.github.com>
2023-10-31 21:12:59 +0800

    Update HowItWorks.vue

--- frontend/src/views/HowItWorks.vue
+++ frontend/src/views/HowItWorks.vue
@@ -29,9 +29,18 @@
 	background-color: #739072;
 }
 
+
 .pr-4.pb-3.pb-md-0 {
 	flex: 2;
 	/* Adjust as needed */
+}
+
+span{
+	color: #3a5a40;
+}
+
+h6{
+	text-decoration: underline;
 }
 </style>
 
@@ -40,13 +49,38 @@
 	<div class="container-fluid pt-3 bg-supergreen pb-3">
 		<div class="row justify-content-center">
 			<div class="col-lg-8">
+				<h2 class="text-center">What is It?</h2>
+				<!-- <img src="../assets/how-it-works.png" alt="How it works" class="img-fluid rounded mx-auto d-block"> -->
+				<p id="whatisit" class="text-center mt-3">PlanIt is a <span>gamified </span>route planner app aimed at encouraging users to reduce transport carbon emissions. This can be done through the adoption of eco-friendly transport options, like walking, or taking public transport over driving.</p>
+			</div>
+			<div class="col-lg-8">
+				<h2 class="text-center">Why PlanIt?</h2>
+				<!-- <img src="../assets/how-it-works.png" alt="How it works" class="img-fluid rounded mx-auto d-block"> -->
+				<h6>Carbon Emissions from Urban Transportation</h6>
+				<ul>
+					<li>Contributes to about 15% of total carbon emissions in Singapore</li>
+					<li>Government aims to reduce transport emissions by 80% (from 2016 levels) by mid-century</li>
+				</ul>
+				<h6>Growing Trend of Gamification</h6>
+				<ul>
+					<li>Gamification market in Singapore projected to expand from 2023 - 2028</li>
+					<li>New and innovative way to drive awareness, motivation, and engagement</li>
+				</ul>
+			</div>
+			<div class="col-lg-8">
 				<h2 class="text-center">How It Works</h2>
 				<!-- <img src="../assets/how-it-works.png" alt="How it works" class="img-fluid rounded mx-auto d-block"> -->
 				<img src="../assets/product_demo_real.gif" alt="How it works" class="img-fluid rounded mx-auto d-block">
 				<p class="text-center mt-3">Our application provides a unique way to navigate your city. It uses advanced algorithms to find the most efficient routes, and it also includes a gamification system to make your journey more fun. Earn badges, level up, and compete with your friends!</p>
 			</div>
 		</div>
-
+		<div class="container-fluid pt-3 bg-supergreen pb-3">
+			<h6>Additional Reading Links</h6>
+			<ol>
+				<li><a href="https://www.lta.gov.sg/content/ltagov/en/newsroom/2022/3/news-releases/reducing-peak-land-transport-emissions-by-80-.html">Land Transport Authority: Reducing Peak Land Transport Emissions by 80%</a></li>
+				<li><a href="https://www.nccs.gov.sg/singapores-climate-action/mitigation-efforts/transport/#:~:text=TRANSPORT,A%20significant%20needle">National Climate Change Secretariat Singapore: Making Walk-Cycle-Ride the Preferred Mode to Travel</a></li>
+			</ol>
+		</div>
 
 	</div>
 </template>
