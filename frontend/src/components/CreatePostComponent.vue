c0d7f5ecc8954efcecdc474990ec7b411ed1733c
ryan99324 <ryanlee99324@gmail.com>
2023-11-03 12:21:26 +0800

    merge

--- frontend/src/components/CreatePostComponent.vue
+++ frontend/src/components/CreatePostComponent.vue
@@ -5,7 +5,6 @@
 			<Editor class="mb-4" editorStyle="height: 100px" v-model="content"></Editor>
 			{{ taggedfriends }}
 
-<<<<<<< HEAD
 						<div class="row mb-3 justify-content-between">
 							<span class="col-1"></span>
     <!--share badges button-->
@@ -61,33 +60,6 @@
     
 							<button class="btn btn-success col-3" type="button"  @click="createPost(user.email)"><font-awesome-icon icon="fa-solid fa-plus-square" /> Create post!</button>
 							<span class="col-1"></span>
-=======
-			<div class="row mb-3 justify-content-between">
-				<span class="col-1"></span>
-				<!--share badges button-->
-				<button class="btn btn-secondary col-3" type="button" @click="badges = !badges" data-bs-target="#badgesOverlay" data-bs-toggle="modal"><font-awesome-icon icon="fa-solid fa-share" /> Share a badge</button>
-
-				<!--modal for share badges-->
-				<div v-if="badges" class="modal fade" id="badgesOverlay" tabindex="-1" aria-labelledby="badgesOverlayLabel" aria-hidden="true">
-					<div class="modal-dialog">
-						<div class="modal-content">
-							<div class="modal-header">
-								<h5 class="modal-title" id="BadgesModalLabel">Badges</h5>
-								<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-							</div>
-							<div class="modal-body">
-								<ul>
-									<li>New recruit <input type="radio" name="badges" value="0" v-model="badge"> </li>
-									<li>Advanced <input type="radio" name="badges" value="1" v-model="badge"> </li>
-									<li>Expert <input type="radio" name="badges" value="2" v-model="badge"> </li>
-
-								</ul>
-							</div>
-							<div class="modal-footer">
-								<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-								<button type="button" class="btn btn-primary" data-bs-dismiss="modal" @click="console.log(badge)">Confirm</button>
-							</div>
->>>>>>> ebcd6e95f3eefe8bd228361d9ea74191f5eac475
 						</div>
 					</div>
 				</div>
