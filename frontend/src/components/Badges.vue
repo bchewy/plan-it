93b987496ec339a47f0f7600448f64dbc8187dec
melodywong23 <melody.wong.2022@scis.smu.edu.sg>
2023-11-04 21:29:34 +0800

    edited badges to call from db instead

--- frontend/src/components/Badges.vue
+++ frontend/src/components/Badges.vue
@@ -34,58 +34,7 @@ export default {
     return {
       isLoading: false,
       length: 3,
-      badges: [
-        {
-          "description": "Congratulations on taking your first step toward a greener planet!",
-          "icon": "green_newbie_icon.png",
-          "id": "badge_1",
-          "milestone": "First carbon footprint calculation",
-          "name": "Green Newbie"
-        },
-        {
-          "description": "You've offset the carbon footprint of your travels for one month!",
-          "icon": "eco_friendly_traveler_icon.png",
-          "id": "badge_2",
-          "milestone": "Offset one month of travel",
-          "name": "Eco-Friendly Traveler"
-        },
-        {
-          "description": "One week of sustainable eating choices. Way to go!",
-          "icon": "foodprint_fighter_icon.png",
-          "id": "badge_3",
-          "milestone": "One week of sustainable food choices",
-          "name": "Foodprint Fighter"
-        },
-        {
-          "description": "You've converted to using solar energy at home.",
-          "icon": "solar_superstar_icon.png",
-          "id": "badge_4",
-          "milestone": "Switch to solar energy",
-          "name": "Solar Superstar"
-        },
-        {
-          "description": "Recycled items for 30 consecutive days!",
-          "icon": "recycle_ranger_icon.png",
-          "id": "badge_5",
-          "milestone": "30 days of recycling",
-          "name": "Recycle Ranger"
-        },
-        {
-          "description": "You've inspired 5 friends to join the app and start tracking their footprint.",
-          "icon": "community_catalyst_icon.png",
-          "id": "badge_6",
-          "milestone": "Refer 5 friends",
-          "name": "Community Catalyst"
-        },
-        {
-          "description": "Achieved a zero-waste lifestyle for one month.",
-          "icon": "zero_waste_warrior_icon.png",
-          "id": "badge_7",
-          "milestone": "One month of zero-waste",
-          "name": "Zero-Waste Warrior"
-        }
-      ]
-
+      badges: []
     };
   },
   created() {
@@ -115,18 +64,19 @@ export default {
       this.length = 3;
     },
     async fetchBadges() {
-      const url = `http://127.0.0.1:5000/users/ez/${this.$route.params.userEmail}/badges`;
-      const headers = {
-        "x-api-key": "PlanItIsTheBestProjectEverXYZ",
-      };
+			const url = `${import.meta.env.VITE_API_ENDPOINT}/users/${encodeURIComponent(this.user.email)}/badges`;
+			const headers = {
+				"x-api-key": "PlanItIsTheBestProjectEverXYZ",
+			};
 
-      try {
-        const response = await axios.get(url, { headers });
-        this.badges = response.data.badges;
-      } catch (error) {
-        console.error("Error fetching badges", error);
-      }
-    },
+			try {
+				const response = await axios.get(url, { headers });
+				this.badges = response.data.badges;
+
+			} catch (error) {
+				console.error("Error fetching badges", error);
+			}
+		},
   },
 };
 </script>
