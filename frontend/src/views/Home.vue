0cb27ee8db2e0b0d9917de699d425862389f4863
Brian Chew <brian@bchewy.com>
2023-11-07 16:20:12 +0800

    Refactoring some code in home.vue

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -3,129 +3,117 @@
     <!-- nav -->
     <NavBar />
   </header>
-
-  <!--username content-->
-  <!-- <section v-if="isAuthenticated" class="text-center col-12 beige-colour p-1">
-    <h2 class="text-muted">Ready to go <span class="text-success">green</span>, {{ user.name }}?</h2>
-  </section> -->
+  <div class="container-fluid p-0">
 
 
-  <!--flexbox thingy!-->
-  <!--section class="text-muted text-center d-flex align-items-center justify-content-center"-->
-  <!-- <Parallaxy :speed="20" :animation="delta => `transform: translate3d(0, ${delta}px, 0);`"> -->
-  <!-- <section class="text-light text-center vh-100 d-flex align-items-center justify-content-center bg-supergreen" style="background-image: url('https://bchewy-images.s3.ap-southeast-1.amazonaws.com/planit.png'); background-size: cover;" v-parallax="0.3"> -->
-  <section class="text-light text-center vh-100 d-flex align-items-center justify-content-center bg-supergreen" style="background-image: url('https://bchewy-images.s3.ap-southeast-1.amazonaws.com/planit.png'); background-size: cover;">
-    <div class="container">
-      <div class="d-sm-flex align-items-center justify-content-center"> <!-- Center vertically and horizontally -->
-        <div class="text-center">
-          <h1><span class="text-supergreen element"></span></h1>
-          <p class="lead">
-            Account for your own carbon footprint for your traffic taken, <br>value-add and commit to reducing your carbon footprint <br>and reduce your personal impact.
-          </p>
+    <section class="text-light text-center vh-100 d-flex align-items-center justify-content-center bg-supergreen" style="background-image: url('https://bchewy-images.s3.ap-southeast-1.amazonaws.com/planit.png'); background-size: cover;">
+      <div class="container">
+        <div class="d-sm-flex align-items-center justify-content-center"> <!-- Center vertically and horizontally -->
+          <div class="text-center">
+            <h1><span class="text-supergreen element"></span></h1>
+            <p class="lead">
+              Account for your own carbon footprint for your traffic taken, <br>value-add and commit to reducing your carbon footprint <br>and reduce your personal impact.
+            </p>
+          </div>
         </div>
       </div>
-    </div>
-    <!--/div-->
-  </section>
-  <!-- </Parallaxy> -->
+      <!--/div-->
+    </section>
+    <!-- </Parallaxy> -->
 
-  <!-- boxes-->
-  <section class="p-5 bg-supergreen">
-    <div class="container">
-      <div class="row text-center">
-        <div class="col-md" v-for="(card, index) in cards" :key="index">
-          <div class="card h-100 beige-colour">
-            <div class="card-body d-flex flex-column justify-content-between">
-              <div class="text-center">
-                <div class="h1 mb-3">
-                  <font-awesome-icon :icon="card.icon"></font-awesome-icon>
+    <!-- boxes-->
+    <section class="p-5 bg-supergreen">
+      <div class="container">
+        <div class="row text-center">
+          <div class="col-md" v-for="(card, index) in cards" :key="index">
+            <div class="card h-100 beige-colour">
+              <div class="card-body d-flex flex-column justify-content-between">
+                <div class="text-center">
+                  <div class="h1 mb-3">
+                    <font-awesome-icon :icon="card.icon"></font-awesome-icon>
+                  </div>
+                  <h3 class="card-title mb-3 ">{{ card.title }}</h3>
+                  <p class="card-text text-muted">{{ card.description }}</p>
+                  <!-- "Learn More" button for each card -->
+                  <button class="btn btn-success" @click="openModal(index)">Learn More</button>
                 </div>
-                <h3 class="card-title mb-3 ">{{ card.title }}</h3>
-                <p class="card-text text-muted">{{ card.description }}</p>
-                <!-- "Learn More" button for each card -->
-                <button class="btn btn-success" @click="openModal(index)">Learn More</button>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  </section>
+    </section>
+
+    <!-- "Learn More" modal -->
+    <LearnMore :visible="currentCardIndex !== -1" :card="currentCardIndex !== -1 ? cards[currentCardIndex] : {}" @close="closeModal" />
+
+    <!-- <div class="bg-supergreen" style="height: 50px;"></div> Adjust the height as needed -->
+
+    <section class="h-25 bg-supergreen" style="background-image: url('https://bchewy-images.s3.ap-southeast-1.amazonaws.com/plan-it/Brown+pathway.jpg'); background-size: cover; min-height: 20em;">
+    </section>
 
 
-  <LearnMore :visible="currentCardIndex !== -1" :card="currentCardIndex !== -1 ? cards[currentCardIndex] : {}" @close="closeModal" />
-
-  <div class="bg-supergreen" style="height: 50px;"></div> <!-- Adjust the height as needed -->
-
-  <!-- Question Accordion -->
-  <section id="questions" class="p-5 bg-supergreen ">
-    <div class="container">
-      <h2 class="text-center mb-4">Frequently Asked Questions</h2>
-      <div class="accordion accordion-flush " id="questions">
-        <!--Item 1-->
-        <div class="accordion-item ">
-          <h2 class="accordion-header ">
-            <button class="accordion-button collapsed beige-colour text-muted" type="button" data-bs-toggle="collapse" data-bs-target="#question-one">
-              <b>1: How does your eco-friendly navigation system work?</b>
-            </button>
-          </h2>
-          <div id="question-one" class="accordion-collapse collapse text-muted" data-bs-parent="#questions">
-            <div class="accordion-body">Our eco-friendly navigation system combines advanced routing algorithms with real-time environmental data. It considers factors like traffic conditions, emissions, and more to suggest the most eco-friendly route. This reduces your carbon footprint and helps you make sustainable travel choices effortlessly.</div>
+    <!-- Question Accordion -->
+    <section id="questions" class="p-5 bg-supergreen ">
+      <div class="container">
+        <h2 class="text-center mb-4">Frequently Asked Questions</h2>
+        <div class="accordion accordion-flush " id="questions">
+          <!--Item 1-->
+          <div class="accordion-item ">
+            <h2 class="accordion-header ">
+              <button class="accordion-button collapsed beige-colour text-muted" type="button" data-bs-toggle="collapse" data-bs-target="#question-one">
+                <b>1: How does your eco-friendly navigation system work?</b>
+              </button>
+            </h2>
+            <div id="question-one" class="accordion-collapse collapse text-muted" data-bs-parent="#questions">
+              <div class="accordion-body">Our eco-friendly navigation system combines advanced routing algorithms with real-time environmental data. It considers factors like traffic conditions, emissions, and more to suggest the most eco-friendly route. This reduces your carbon footprint and helps you make sustainable travel choices effortlessly.</div>
+            </div>
           </div>
-        </div>
-        <!--Item 2-->
-        <div class="accordion-item">
-          <h2 class="accordion-header">
-            <button class="accordion-button collapsed beige-colour text-muted" type="button" data-bs-toggle="collapse" data-bs-target="#question-two">
-              <b>2: What types of rewards can I earn through the Incentivising Public Transport feature?</b>
-            </button>
-          </h2>
-          <div id="question-two" class="accordion-collapse collapse text-muted " data-bs-parent="#questions">
-            <div class="accordion-body">With our Incentivising Public Transport feature, you can earn various rewards such as discounts on future journeys, eco-friendly badges, and even contributions to environmental causes. The more you choose public transport, the greater your rewards become.</div>
+          <!--Item 2-->
+          <div class="accordion-item">
+            <h2 class="accordion-header">
+              <button class="accordion-button collapsed beige-colour text-muted" type="button" data-bs-toggle="collapse" data-bs-target="#question-two">
+                <b>2: What types of rewards can I earn through the Incentivising Public Transport feature?</b>
+              </button>
+            </h2>
+            <div id="question-two" class="accordion-collapse collapse text-muted " data-bs-parent="#questions">
+              <div class="accordion-body">With our Incentivising Public Transport feature, you can earn various rewards such as discounts on future journeys, eco-friendly badges, and even contributions to environmental causes. The more you choose public transport, the greater your rewards become.</div>
+            </div>
           </div>
-        </div>
-        <!--Item 3-->
-        <div class="accordion-item">
-          <h2 class="accordion-header">
-            <button class="accordion-button collapsed beige-colour text-muted" type="button" data-bs-toggle="collapse" data-bs-target="#question-three">
-              <b> 3: Can I use your platform internationally, or is it limited to specific regions?</b>
-            </button>
-          </h2>
-          <div id="question-three" class="accordion-collapse collapse " data-bs-parent="#questions">
-            <div class="accordion-body">Our eco-friendly navigation platform is currently tailored to Singapore. While we're focused on providing the best eco-friendly navigation experience in Singapore, we have plans to expand to other regions in the future. Stay tuned for updates on our international availability!</div>
+          <!--Item 3-->
+          <div class="accordion-item">
+            <h2 class="accordion-header">
+              <button class="accordion-button collapsed beige-colour text-muted" type="button" data-bs-toggle="collapse" data-bs-target="#question-three">
+                <b> 3: Can I use your platform internationally, or is it limited to specific regions?</b>
+              </button>
+            </h2>
+            <div id="question-three" class="accordion-collapse collapse " data-bs-parent="#questions">
+              <div class="accordion-body">Our eco-friendly navigation platform is currently tailored to Singapore. While we're focused on providing the best eco-friendly navigation experience in Singapore, we have plans to expand to other regions in the future. Stay tuned for updates on our international availability!</div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </section>
+    </section>
 
-  <div class="bg-supergreen" style="height: 80px;"></div> <!-- Adjust the height as needed -->
 
-  <!-- refer friend now-->
-  <section class="text-muted bg-supergreen ">
-    <div class="container">
-      <div class="d-md-flex justify-content-between align-items-center mb-3 mb-md-0">
-        <h4 class="news-input">Refer a friend now!</h4> <!-- Apply the "news-input" class -->
-        <div class="input-group news-input">
-          <input type="text" class="form-control news-input" placeholder="Friend's Email Address" aria-label="Friend's Email Address" aria-describedby="button-addon2">
-          <button class="btn btn-lg beige-colour" id="button-addon2">Send</button>
+    <!-- refer friend now-->
+    <!-- <section class="text-muted bg-supergreen">
+      <div class="container">
+        <div class="d-md-flex justify-content-between align-items-center mb-3 mb-md-0">
+          <h4 class="news-input">Refer a friend now!</h4>
+          <div class="input-group news-input">
+            <input type="text" class="form-control news-input" placeholder="Friend's Email Address" aria-label="Friend's Email Address" aria-describedby="button-addon2">
+            <button class="btn btn-lg beige-colour" id="button-addon2">Send</button>
+          </div>
         </div>
       </div>
-    </div>
-  </section>
+    </section> -->
 
-  <!-- Add space between "Refer a friend" section and footer -->
-  <div class="bg-supergreen" style="height: 71px;"></div> <!-- Adjust the height as needed -->
+    <!-- Add space between "Refer a friend" section and footer -->
 
-  <footer class="p-5 text-center position-relative" style="background-color: rgb(209, 244, 209);">
-    <!-- footer -->
-    <div class="container">
-      <p class="lead">Copyright &copy; 2023 wad2winners</p>
-      <a href="#" class="position-absolute bottom-0 end-0 p-5 text-center">
-        <i class="bi bi-arrow-up-circle h1 text-success" style="background-color: transparent;"></i>
-      </a>
-    </div>
-  </footer>
+    <!-- Footer -->
+    <!-- <Footer /> -->
+  </div>
 </template>
 
 <script>
@@ -134,6 +122,7 @@ import { useAuth0 } from '@auth0/auth0-vue';
 import LearnMore from '../components/LearnMore.vue';
 import { ref } from 'vue';
 import Typed from 'typed.js';
+import Footer from '../components/Footer.vue'
 // import Parallaxy from '@lucien144/vue3-parallaxy';
 
 
@@ -148,7 +137,7 @@ export default {
   components: {
     NavBar,
     LearnMore,
-    // Parallaxy,
+    Footer,
   },
   mounted() {
     const element = document.querySelector('.element');
