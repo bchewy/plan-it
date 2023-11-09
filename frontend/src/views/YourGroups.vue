15160ceb6646ab042e21cc1b78c54d1c03ac5ec7
Brian Chew <brian@bchewy.com>
2023-11-09 23:22:06 +0800

    pushing

--- frontend/src/views/YourGroups.vue
+++ frontend/src/views/YourGroups.vue
@@ -13,77 +13,74 @@
                 <span class="header text-muted" style="font-weight: bold;">Your Groups </span>
             </div>
         </div>
-        <div v-if="loading">Loading...</div>
-
-        <div v-else v-for="(group, index) in groups" :key="index" class="row" style="background-color: #cbdbb7;">
-            <!-- {{ groups }} -->
-            <br><br>
-            <div :key="group._id" class="col-md-4 mb-4">
-
-                <!-- {{ group }} -->
-
-                <div class="card">
-                    <img class="card-img-top" :src="group.group_image" alt="Group Image" v-if="group.group_image">
-                    <div class="card-body">
-
-                        <h5 class="card-title fs-2 text-center">{{ group.name }}</h5>
-                        <br>
-
-                        <p class="card-text"> <span class="fw-bold">Owner:</span> {{ group.owner_email }}</p>
-                        <br>
-
-                        <p class="card-text"> <span class="fw-bold">Members:</span></p>
-                        <ul class="list-group">
-                            <li v-for="member in group.members " class="list-group-item bg-light">{{ member }}</li>
-                        </ul> <br>
-
-                        <div v-if="group.badges">
-                            <p class="card-text"><span class="fw-bold">Badges:</span></p>
+        <div class="row" style="background-color: #cbdbb7;">
 
 
-                            <div v-for="badge in group.badges" class="col-11 bg-light text-center rounded border mb-2">
-                                <img :src="badge.image" class="w-75 rounded-circle mb-3 mt-3"><br>
-                                <button class="btn btn-secondary mb-3" data-bs-toggle="modal" :data-bs-target="'#a' + badge._id">More info</button>
+            <div v-if="loading">Loading...</div>
 
-                                <div class="modal fade" :id="'a' + badge._id" tabindex="-1" :aria-labelledby="badge._id" aria-hidden="true">
-                                    <div class="modal-dialog">
-                                        <div class="modal-content">
-                                            <div class="modal-header">
-                                                <h5 class="modal-title" :id="badge.name">Badge Info</h5>
-                                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                            </div>
-                                            <div class="modal-body">
-                                                <div class="row">
-                                                    <div class="col justify-content-end">
-                                                        <img :src="badge.image" class="w-100">
-                                                    </div>
-                                                    <div class="col align-items-center d-flex">
-                                                        <div> <span class="fw-bold fs-4">{{ badge.name }} </span><br><br> {{ badge.description }} <br><br><strong>Milestone:</strong> {{ badge.milestone }}</div>
+            <div v-else v-for="(group, index) in groups" :key="index" class="col-md-4 mb-4" style="background-color: #cbdbb7;">
+                <div :key="group._id">
 
-                                                    </div>
+                    <div class="card">
+                        <img class="card-img-top" :src="group.group_image" alt="Group Image" v-if="group.group_image">
+                        <div class="card-body">
+
+                            <h5 class="card-title fs-2 text-center">{{ group.name }}</h5>
+                            <br>
+
+
+                            <p class="card-text"> <span class="fw-bold">Members:</span></p>
+                            <ul class="list-group">
+                                <li v-for="member in group.members " class="list-group-item bg-light">{{ member }}</li>
+                            </ul> <br>
+
+                            <div v-if="group.badges">
+                                <p class="card-text"><span class="fw-bold">Badges:</span></p>
+
+
+                                <div v-for="badge in group.badges" class="col-11 bg-light text-center rounded border mb-2">
+                                    <img :src="badge.image" class="w-75 rounded-circle mb-3 mt-3"><br>
+                                    <button class="btn btn-secondary mb-3" data-bs-toggle="modal" :data-bs-target="'#a' + badge._id">More info</button>
+
+                                    <div class="modal fade" :id="'a' + badge._id" tabindex="-1" :aria-labelledby="badge._id" aria-hidden="true">
+                                        <div class="modal-dialog">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <h5 class="modal-title" :id="badge.name">Badge Info</h5>
+                                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                                 </div>
-                                                <div class="modal-footer">
+                                                <div class="modal-body">
+                                                    <div class="row">
+                                                        <div class="col justify-content-end">
+                                                            <img :src="badge.image" class="w-100">
+                                                        </div>
+                                                        <div class="col align-items-center d-flex">
+                                                            <div> <span class="fw-bold fs-4">{{ badge.name }} </span><br><br> {{ badge.description }} <br><br><strong>Milestone:</strong> {{ badge.milestone }}</div>
 
-                                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                                        </div>
+                                                    </div>
+                                                    <div class="modal-footer">
+
+                                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
+
                                     </div>
 
                                 </div>
 
+
                             </div>
-
-
-                        </div>
-                        <div v-else>
-                            <p class="card-text"><span class="fw-bold">Badges:</span> None</p>
+                            <div v-else>
+                                <p class="card-text"><span class="fw-bold">Badges:</span> None</p>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-
 
 
     </div>
