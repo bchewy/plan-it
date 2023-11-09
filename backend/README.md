12f8f9b424d8177bf038008dad583785493f8fdc
Brian Chew <brian@bchewy.com>
2023-11-09 17:16:53 +0800

    removed mongoURI for prod

--- backend/README.md
+++
@@ -1,5 +0,0 @@
-
-### Skill-tree local installation
-1. ensure you have docker! (you can just install docker desktop) https://docs.docker.com/compose/install/
-2. navigate to the skilltree_dashui/local directory on your terminal
-3. run `docker-compose up`
--- backend/config.py
+++ backend/config.py
@@ -1,3 +1,7 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 class Config:
     DEBUG = True
-    MONGO_URI = "mongodb://admin:T%3D5T1i%2BiWUMac4sWAj!9@api.bchwy.com:27017/?authMechanism=DEFAULT&directConnection=true"
+    MONGO_URI = os.getenv("MONGO_URI")
